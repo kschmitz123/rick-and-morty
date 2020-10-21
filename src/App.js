91a78fba16ea30452294c871f280db29bde6ba3c
kschmitz123 <kathrin_schmitz93@gmx.de>
2020-10-21 14:12:20 +0200

    Create random number

--- src/App.js
+++ src/App.js
@@ -16,6 +16,11 @@ function App() {
     className: "main",
   });
 
+  function getRandom(min, max) {
+    return Math.floor(Math.random() * (max - min) + min);
+  }
+  console.log(getRandom(1, 671));
+
   async function getCharacters() {
     for (let i = 1; i <= 10; i++) {
       const character = await getCharacterByID(i);
